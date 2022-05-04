0fe6744692e0ace7bb3a0dd6fabc7ace4128ca12
TmKrvc <93292083+TmKrvc@users.noreply.github.com>
2022-05-04 17:22:41 +0400

    Initial commit

---
+++ src/main/java/com/example/springbootdemo/service/UserService.java
@@ -0,0 +1,36 @@
+package com.example.springbootdemo.service;
+
+import com.example.springbootdemo.model.User;
+import com.example.springbootdemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findBuId(Long id) {
+        return userRepository.findById(id).orElse(null);
+        //getOne(id)
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
